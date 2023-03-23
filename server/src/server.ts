66b4f421b4d6f2deec614adbcff1238a2d0a86a8
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-03-23 20:48:43 -0300

    servidor error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,66 +1,18 @@
-import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
-import { Server, IncomingMessage, ServerResponse } from 'http';
-import { load } from 'ts-dotenv';
-//import cors from '@fastify/cors';
-import { PrismaClient } from '@prisma/client';
+// Require the framework and instantiate it
+const fastify = require('fastify')({ logger: true })
 
-const prisma = new PrismaClient();
+// Declare a route
+fastify.get('/', async (request: any, reply: any) => {
+  return { hello: 'world' }
+})
 
-const env = load({
-  PORT: Number,
-  DATABASE_URL: String,
-});
-
-const server: FastifyInstance = Fastify({});
-
-const opts: RouteShorthandOptions = {
-  schema: {
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          msg: {
-            type: 'string'
-          }
-        }
-      }
-    }
-  }
-}
-/*
-server.register(cors, {
-  // put your options here
-});
-*/
-server.get('/', opts, async (request, reply) => {
-  return { msg: 'it worked!' };
-});
-
-server.post('/user', async (resquest, reply) => {
-  const user = await prisma.user.create({
-    data: {
-      name: 'Fulano de Tal',
-      email: 'fulano@email.com',
-    },
-  })
-  console.log(user)
-  return reply.status(200).send({ user });
-});
-
+// Run the server!
 const start = async () => {
   try {
-    const PORT = process.env.PORT;
-    console.log(PORT);
-    await server.listen({ port: 3000 });
-
-    const address = server.server.address();
-    const port = typeof address === 'string' ? address : address?.port;
-
-    console.log('Server inited...');
+    await fastify.listen({ port: 3000 })
   } catch (err) {
-    server.log.error(err);
-    process.exit(1);
+    fastify.log.error(err)
+    process.exit(1)
   }
 }
-console.log('Server initing...');
-start();
+start()
