c232bbbaa13fdc9244ce81f5f6f1fc1465f3c265
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-03-23 20:57:14 -0300

    servidor error

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ fastify.get('/', async (request: any, reply: any) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port: process.env.PORT })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
