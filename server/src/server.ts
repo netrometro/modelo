f10ce52aacc14b207b23e7ff9cdbcbc5985f1ba5
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-03-23 21:06:15 -0300

    servidor error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,1 @@
-// Require the framework and instantiate it
-const fastify = require('fastify')({ logger: true })
-
-// Declare a route
-fastify.get('/', async (request: any, reply: any) => {
-  return { hello: 'world' }
-})
-
-// Run the server!
-const start = async () => {
-  try {
-    await fastify.listen({ port: process.env.PORT })
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
+console.log(process.env.PORT)
