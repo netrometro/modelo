9d0c69faea3e88d54e5cb51df5e40a3fa496eefc
Ewerton Mendonça <netrometro@yahoo.com.br>
2023-03-23 21:25:23 -0300

    ...

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,64 @@
-console.log(process.env.PORT)
+import Fastify, { FastifyInstance, RouteShorthandOptions } from 'fastify';
+import { Server, IncomingMessage, ServerResponse } from 'http';
+import { load } from 'ts-dotenv';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const env = load({
+  PORT: Number,
+  DATABASE_URL: String,
+});
+
+const server: FastifyInstance = Fastify({});
+
+const opts: RouteShorthandOptions = {
+  schema: {
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          msg: {
+            type: 'string'
+          }
+        }
+      }
+    }
+  }
+}
+
+server.register(cors, {
+  // put your options here
+});
+
+server.get('/', opts, async (request, reply) => {
+  return { msg: 'it worked!' };
+});
+
+server.post('/user', async (resquest, reply) => {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Fulano de Tal',
+      email: 'fulano@email.com',
+    },
+  })
+  console.log(user)
+  return reply.status(200).send({ user });
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: env.PORT });
+
+    const address = server.server.address();
+    const port = typeof address === 'string' ? address : address?.port;
+
+    console.log('Server inited...');
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+}
+console.log('Server initing...');
+start();
